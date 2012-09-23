48045eb093f79df5e5f98eb932daed1281460804
Vitaly _Vi Shukela <vi0oss@gmail.com>
2012-09-23 03:04:13 +0300

    Improve the test.sh

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ function setup() {
     fusermount -u m 2> /dev/null || true
     mkdir -p m
     dd if=/dev/zero of=s bs=1024 count=1024 2> /dev/null
-    BLOCK_SIZE=128 ./randomallocfs s m 2> /dev/null
+    ./randomallocfs s m 2> /dev/null
 }
 
 function teardown() {
@@ -40,6 +40,10 @@ function measuretransferred() {
     '
 }
 
+function tests() {
+
+echo "BLOCK_SIZE=$BLOCK_SIZE"
+
 echo "Dummy test"
 setup
 diff -u - <(find m -printf '%y %M %n %s %P %Cs %As %Ts\n') <<\EOF
@@ -63,7 +67,7 @@ setup
 echo qqq > m/qqq
 mkdir m/ddd
 fusermount -u m
-BLOCK_SIZE=128 ./randomallocfs s m
+./randomallocfs s m
 diff -u - <(find m -printf '%y %M %n %s %P %Cs %As %Ts\n') <<\EOF
 d drwxr-x--- 0 0  0 0 0
 f -rwxr-x--- 0 4 qqq 0 0 0
@@ -78,7 +82,7 @@ SIZE=`find m/file -printf '%s'`
 echo "  Uses space: " $((SIZE*100/1024/1024)) "%"
 fusermount -u m
 sleep 1 # XXX
-BLOCK_SIZE=128 ./randomallocfs s m
+./randomallocfs s m
 SIZE2=`find m/file -printf '%s'`
 test "$SIZE" == "$SIZE2"
 diff -u <(yes 123456789 | nl | chopfile "$SIZE") m/file
@@ -87,7 +91,7 @@ teardown
 echo "Sudden shutdown test"
 dd if=/dev/zero of=s bs=1024 count=102400 2> /dev/null
 mkdir -p m
-BLOCK_SIZE=128 ./randomallocfs s m 2> /dev/null
+./randomallocfs s m 2> /dev/null
 PID=`pgrep -f 'randomallocfs s m'`
 yes 123456789 | nl | measuretransferred > m/file 2> stats&
 sleep 2
@@ -95,7 +99,7 @@ sleep 2
 pkill -9 -f 'randomallocfs s m'
 sleep 1
 fusermount -u m
-BLOCK_SIZE=128 ./randomallocfs s m
+./randomallocfs s m
 SIZE=$(<stats)
 rm stats
 SIZE2=`find m/file -printf '%s'`
@@ -103,6 +107,17 @@ echo "    Lost bytes: " $((SIZE-SIZE2)) " of $SIZE"
 diff -u <(yes 123456789 | nl | chopfile "$SIZE2") m/file
 teardown
 
+}
+
+export BLOCK_SIZE=128
+tests
+export BLOCK_SIZE=1024
+tests
+export BLOCK_SIZE=4096
+tests
+export BLOCK_SIZE=65536
+tests
+export BLOCK_SIZE=128
 
 
 echo "Filesystem name test"
@@ -115,3 +130,5 @@ f -rwxr-x--- 0 4 qqq fuse.randomallocfs 0 0 0
 d drwxr-x--- 0 0 ddd fuse.randomallocfs 0 0 0
 EOF
 teardown
+
+echo "All tests finished."
