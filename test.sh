a5dd0bb1c8d9583636bdc9973d38a58e07976aa2
Vitaly _Vi Shukela <vi0oss@gmail.com>
2012-09-28 20:08:02 +0300

    Add complex multi-branch test

--- test.sh
+++ test.sh
@@ -12,6 +12,10 @@ function setup() {
     echo "2test" | ./chaoticfs s m  > /dev/null 2> /dev/null
 }
 
+function um() {
+    fusermount -u m || { sleep 2 && fusermount -u m; } || { sleep 10 && fusermount -u m; }
+}
+
 function teardown() {
     fusermount -u m 2> /dev/null || true
     rm -f s
@@ -80,13 +84,77 @@ setup
 yes ABCDEFGH | nl | cat > m/file || true
 SIZE=`find m/file -printf '%s'`
 echo "  Uses space: " $((SIZE*100/1024/1024)) "%"
-fusermount -u m
+um
 sleep 1 # XXX
 echo "2test" | ./chaoticfs s m > /dev/null
 SIZE2=`find m/file -printf '%s'`
 test "$SIZE" == "$SIZE2"
 diff -u <(yes ABCDEFGH | nl | chopfile "$SIZE") m/file
 teardown
+
+echo "Complex test"
+echo "   prepare data"
+dd if=/dev/zero of=s bs=1024 count=102400 2> /dev/null
+rm -Rf dataset1 dataset2 dataset3
+mkdir -p dataset1 dataset2 dataset3
+dd if=/dev/urandom bs=1337 count=128 of=randomlet 2> /dev/null
+for i in {1..10}; do
+    for j in `seq 1 $i`; do
+        echo "ds1 $j" >> dataset1/$i.dat
+        cat randomlet >> dataset1/$i.dat
+        echo "2 $j" >> dataset2/$i.dat
+        cat randomlet >> dataset2/$i.dat
+        echo "3333333333333333333333333333333 $j" >> dataset3/$i.dat
+        cat randomlet >> dataset3/$i.dat
+    done
+done
+(cd dataset1 && sha1sum *.dat > SHA1SUMS;)
+(cd dataset2 && sha1sum *.dat > SHA1SUMS;)
+(cd dataset3 && sha1sum *.dat > SHA1SUMS;)
+
+echo "   copy data"
+mkdir -p m
+echo "2test" | ./chaoticfs s m > /dev/null 2> /dev/null
+cp -R dataset1 m/
+um
+
+mkdir -p m
+echo "2test,3test" | ./chaoticfs s m > /dev/null 2> /dev/null
+cp -R dataset2 m/
+um
+
+mkdir -p m
+echo "2test,3test" | ./chaoticfs s m > /dev/null 2> /dev/null
+cp -R dataset2 m/
+um
+
+mkdir -p m
+echo "2test,3test,4test" | ./chaoticfs s m > /dev/null 2> /dev/null
+cp -R dataset3 m/
+um
+
+mkdir -p m
+echo "4test,2test,3test" | ./chaoticfs s m > /dev/null 2> /dev/null
+cp -R dataset2 m/
+um
+
+echo "   check data"
+echo "2test" | ./chaoticfs s m > /dev/null 2> /dev/null
+cmp dataset1/SHA1SUMS m/dataset1/SHA1SUMS
+(cd m/dataset1/ && sha1sum --quiet -c SHA1SUMS)
+um
+
+echo "3test" | ./chaoticfs s m > /dev/null 2> /dev/null
+cmp dataset2/SHA1SUMS m/dataset2/SHA1SUMS
+(cd m/dataset2/ && sha1sum --quiet -c SHA1SUMS)
+um
+
+echo "4test" | ./chaoticfs s m > /dev/null 2> /dev/null
+cmp dataset3/SHA1SUMS m/dataset3/SHA1SUMS
+(cd m/dataset3/ && sha1sum --quiet -c SHA1SUMS)
+um
+
+rm -Rf dataset1 dataset2 dataset3 m
 
 
 echo "Sudden shutdown test"
@@ -103,7 +171,7 @@ cat testfile | measuretransferred > m/file 2> stats&
 sleep 20 # adjust this it your computer is too fast
 pkill -9 -f 'chaoticfs s m'
 sleep 1
-fusermount -u m || { sleep 2 && fusermount -u m; } || { sleep 10 && fusermount -u m; }
+um
 echo "2test" | ./chaoticfs s m > /dev/null
 SIZE=$(<stats)
 if [ ! -e m/file ]; then
@@ -121,10 +189,10 @@ teardown
 
 }
 
-
+BLOCK_SIZE=8192 tests
+BLOCK_SIZE=4096 tests
 BLOCK_SIZE=128 NO_O_DIRECT=y tests
 BLOCK_SIZE=1024 NO_O_DIRECT=y tests
-BLOCK_SIZE=4096 tests
 BLOCK_SIZE=65536 tests
 
 
